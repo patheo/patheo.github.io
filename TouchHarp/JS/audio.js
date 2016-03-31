1fffcc99b5dcc1d37caabb833fd5b18c2b2bdc4f
patheo <pierre-adrientheo@MacBook-Pro-de-Pierre-Adrien.local>
2016-03-31 17:36:22 +0200

    Correction orthographe page d’accueil et suppression de l’ouverture du micro dans la version Harp.

--- TouchHarp/JS/audio.js
+++ TouchHarp/JS/audio.js
@@ -1,12 +1,5 @@
-/*
-  * Lancement de la web audio API.
-  * Connection du microphone vers le noeud faust.
-  * Connection du noeud faust vers la sortie audio.
- */
-
 var isWebKitAudio = (typeof (webkitAudioContext) !== "undefined");
 var audio_context = (isWebKitAudio) ? new webkitAudioContext() : new AudioContext();
-var audio_input = null;
 var effetFaust = null;
 var faustsvg = null;
 var dsp_code = null;
@@ -18,48 +11,16 @@ function changeBufferSize(buffer_size)
     startNeweffetFaust(new_buffer_size);
 }
 
-// Audio input handling
-
-function activateAudioInput()
-{
-    if (!navigator.getUserMedia) {
-        navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia ;
-    }
-
-    if (navigator.getUserMedia) {
-        navigator.getUserMedia({audio:true}, getDevice, function(e) {
-                alert('Error getting audio input');
-                console.log(e);
-        });
-    } else {
-        alert('Audio input API not available');
-    }
-}
-
-function getDevice(device)
-{
-    // Create an AudioNode from the stream.
-    audio_input = audio_context.createMediaStreamSource(device);
-
-    // Connect it to the destination.
-    audio_input.connect(effetFaust.getProcessor());
-}
 
 function starteffetFaustAux(buffer_size)
 {
     console.log("startAudioFX %d", buffer_size);
     if (effetFaust) {
-        if (audio_input) {
-            audio_input.disconnect(effetFaust.getProcessor());
-        }
         _f4u$t.hard_delete(faustsvg);
         effetFaust.stop();
         effetFaust.destroy();
     }
     effetFaust = faust.effetFaust(audio_context, buffer_size);
-    if (effetFaust.getNumInputs() > 0) {
-        activateAudioInput();
-    }
     effetFaust.start();
     console.log(effetFaust.json());
     console.log(effetFaust.controls());
